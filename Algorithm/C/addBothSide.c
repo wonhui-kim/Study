7a9407d892aad92e2b7bf965526c9091d08e9c92
Wonhui Kim <96123303+wonhui-kim@users.noreply.github.com>
2022-01-02 09:49:57 +0900

    add description addBothSide.c

--- Algorithm/C/addBothSide.c
+++ Algorithm/C/addBothSide.c
@@ -1,3 +1,13 @@
+/*
+ make array(b) with sum both side of the inputted array(a), arraySize:10
+ (a)  0    1    2    3    4    5    6    7    8    9
+ (b) 0+1  0+2  1+3  2+4  3+5  4+6   ...
+ 
+ ex) input: 2 3 1 0 0 1 1 0 2
+     output:5 3 3 1 1 1 1 3 2 
+*/
+
+
 #include <stdio.h>
 #define NUMBER 10
 

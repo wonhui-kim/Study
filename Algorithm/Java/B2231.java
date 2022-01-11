867189cdadf973ddf2210ca62fa845842ecc2763
Wonhui Kim <96123303+wonhui-kim@users.noreply.github.com>
2022-01-11 22:22:58 +0900

    boj 2231 (brute-force search) commit

---
+++ Algorithm/Java/B2231.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//백준 2231 분해합 (브루트포스)
+
+public class B2231 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.println(cal(n));
+	}
+	
+	public static int cal(int n) {
+		
+		int m = 0;
+		
+		for(int i = 0; i < n; i++) {
+			int sum = 0;
+			int temp = i;
+			
+			while(temp > 0) {
+				sum += temp % 10;
+				temp /= 10;
+			}
+			
+			if(sum + i == n) {
+				m = i;
+				break;
+			}
+		}
+		
+		return m;
+	}
+}

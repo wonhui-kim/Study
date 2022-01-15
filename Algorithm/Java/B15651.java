3d0d927a0e8582680a29b08dcbe4049267493430
Wonhui Kim <96123303+wonhui-kim@users.noreply.github.com>
2022-01-15 23:40:26 +0900

    boj 15651 (back tracking) commit

---
+++ Algorithm/Java/B15651.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+//백준 15651 N과M(3) 백트래킹
+
+public class B15651 {
+	public static int n, m;
+	public static char[] result;
+	public static StringBuilder sb;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		result = new char[m*2];
+		Arrays.fill(result, ' ');
+		
+		bt(0);
+		System.out.print(sb);
+	}
+	
+	public static void bt(int depth) {
+		if(depth == m) {
+			sb.append(result).append('\n');
+			return;
+		}
+		
+		for(int i = 1; i <= n; i++) {
+			result[depth*2] = (char)(i + '0');
+			bt(depth+1);
+		}
+	}
+
+}

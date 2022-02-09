3fa0f4dac6459b54c1485b1a858db6760f79abca
Wonhui Kim <96123303+wonhui-kim@users.noreply.github.com>
2022-02-09 22:52:25 +0900

    boj 11022 (for) commit

---
+++ Algorithm/Java/B11022.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11022 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= t; i++) {
+			String[] temp = br.readLine().split(" ");
+			
+			int a = Integer.parseInt(temp[0]);
+			int b = Integer.parseInt(temp[1]);
+			
+			sb.append("Case #"+ i + ": ").append(a + " + " + b + " = ").append(a+b).append('\n');
+		}
+		
+		System.out.print(sb);
+	}
+}

c1ea60b03600f974213fbfa3647a2519b53204fd
Wonhui Kim <96123303+wonhui-kim@users.noreply.github.com>
2022-02-14 22:19:49 +0900

    boj 2438 (for) commit
    String s = "*";
    String repeated = s.repeat(3);
    repeated.equals("***");

---
+++ Algorithm/Java/B2438.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2438 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= n; i++) {
+			sb.append("*".repeat(i)).append('\n');
+		}
+		System.out.print(sb);
+	}
+}

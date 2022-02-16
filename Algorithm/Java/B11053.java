7b947e4fd676e826b43dbe30241d7be3276d534d
Wonhui Kim <96123303+wonhui-kim@users.noreply.github.com>
2022-02-16 21:06:04 +0900

    boj 11053 (dp) commit

---
+++ Algorithm/Java/B11053.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+//백준 11053 가장 긴 증가하는 부분수열 (dp)
+
+public class B11053 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int[] dp = new int[n];
+		
+		String[] temp = br.readLine().split(" ");
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(temp[i]);
+		}
+		
+		for(int i = 0; i < n; i++) {
+			dp[i] = 1;
+			for(int j = 0; j < i; j++) {
+				if(arr[j] < arr[i] && dp[i] <= dp[j]) {
+					dp[i] = dp[j] + 1;
+				}
+			}
+		}
+		
+		int max = -1;
+		for(int i = 0; i < n; i++) {
+			max = Math.max(max, dp[i]);
+		}
+		
+		bw.write(String.valueOf(max));
+		bw.flush();
+	}
+}

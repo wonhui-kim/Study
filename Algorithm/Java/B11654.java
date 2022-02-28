de84df1fc5b663d557e3cca7e19476fe87372928
Wonhui Kim <96123303+wonhui-kim@users.noreply.github.com>
2022-02-28 21:42:32 +0900

    boj 11654 (String) commit
    //String to char
    
    String s = "A";
    char c = s.charAt(0); //A

---
+++ Algorithm/Java/B11654.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11654 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print((int)br.readLine().charAt(0));
+	}
+}

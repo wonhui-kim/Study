1207a971f2041917efcaa93f4155168031ba6c1a
Wonhui Kim <96123303+wonhui-kim@users.noreply.github.com>
2022-03-03 17:46:28 +0900

    boj 10926 (input) commit

---
+++ Algorithm/Java/B10926.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B10926 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.write(br.readLine() + "??!");
+		bw.flush();
+	}
+}

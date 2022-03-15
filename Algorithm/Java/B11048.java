bc8186ab5e6f051b8ca2eb01b7a57eecf3fa7510
Wonhui Kim <96123303+wonhui-kim@users.noreply.github.com>
2022-03-15 20:49:26 +0900

    re_boj 11048 (dp) commit

--- Algorithm/Java/B11048.java
+++ Algorithm/Java/B11048.java
@@ -3,25 +3,26 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-//πÈ¡ÿ 11048 ¿Ãµø«œ±‚ (dp)
+//Î∞±Ï§Ä 111048 Ïù¥ÎèôÌïòÍ∏∞ (dp)
 
 public class B11048 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		String[] temp = br.readLine().split(" ");
-		int n = Integer.parseInt(temp[0]);
-		int m = Integer.parseInt(temp[1]);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] maze = new int[n+1][m+1];
 		
-		int max = -1;
-		int[][] maze = new int[n+1][m+1];		
-		for(int i = 1; i <= n; i++) { //3
-			temp = br.readLine().split(" ");
-			for(int j = 1; j <= m; j++) { //4
-				maze[i][j] = Integer.parseInt(temp[j-1]);
-				max = Math.max(maze[i-1][j-1], Math.max(maze[i][j-1], maze[i-1][j]));
+		int max = 0;
+		for(int i = 1; i <= n; i++) { //ÎØ∏Î°ú ÏûÖÎ†•Î∞õÍ∏∞
+			st = new StringTokenizer(br.readLine());
+			for(int j = 1; j <= m; j++) {
+				maze[i][j] = Integer.parseInt(st.nextToken());
+				max = Math.max(maze[i-1][j-1], Math.max(maze[i-1][j], maze[i][j-1]));
 				maze[i][j] += max;
 			}
 		}

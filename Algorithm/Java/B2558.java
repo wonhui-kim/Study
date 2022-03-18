96497977c18198797038b5f0a0f51b54bb592271
Wonhui Kim <96123303+wonhui-kim@users.noreply.github.com>
2022-03-18 22:47:49 +0900

    boj 2558 (input/output) commit

---
+++ Algorithm/Java/B2558.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B2558 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int answer = Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine());
+		bw.write(String.valueOf(answer));
+		bw.flush();
+	}
+}

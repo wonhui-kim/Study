63bd113c5bb826434794be62a1219b98a6737576
Wonhui Kim <96123303+wonhui-kim@users.noreply.github.com>
2022-03-20 16:08:07 +0900

    boj 2579 (dp) commit

---
+++ Algorithm/Java/B2579.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+//백준 2579 계단 오르기 (dp)
+
+public class B2579 {
+	public static int[] arr;
+	public static Integer[] dp;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		arr = new int[n+1];
+		for(int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		dp = new Integer[n+1];
+		dp[0] = 0;
+		dp[1] = arr[1];
+		
+		if(n > 1) {
+			dp[2] = arr[1] + arr[2];
+		}
+		
+		bw.write(String.valueOf(recur(n)));
+		bw.flush();
+	}
+	
+	public static int recur(int n) {
+		if(dp[n] == null) {
+			dp[n] = Math.max(recur(n-2), recur(n-3)+arr[n-1]) + arr[n];
+		}
+		return dp[n];
+	}
+}

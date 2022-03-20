47a352ddce01477cac6c1bc345f7b9d8ff3a2144
Wonhui Kim <96123303+wonhui-kim@users.noreply.github.com>
2022-03-20 15:20:43 +0900

    boj 10817 (sorting) commit

---
+++ Algorithm/Java/B10817.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B10817 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[3];
+		for(int i = 0; i < 3; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		bw.write(String.valueOf(arr[1]));
+		bw.flush();
+	}
+}
